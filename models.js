0258d9302d67cc21e8dd5403d2ce68eba5239907
Glitch (lily-chemistry) <none>
2019-01-18 22:15:33 +0000

    👮🖼 Checkpoint ./models.js:1652815/937

--- models.js
+++ models.js
@@ -1,7 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI);
+
+const CounterSchema = new mongoose.Schema({
+  seq: { type: Number, default: 0}
+});
 
 const UrlSchema = new mongoose.Schema({
   "original_url": {type: "string", required: true}
-})
+});
+
+const counter = mongoose.model('counter', CounterSchema);
+
+UrlSchema.pre('save', function(next) {
+  const doc = this;
+  
+  counter.findByIdAndUpdateAsync({_id: 'entityId'}, 
+                                 {$inc: {seq: 1}}, 
+                                 {new: true, upsert: true}
+  .then(function(count){
+    doc.short_url = count.seq;
+    next()
+  })
+  .catch(function(error) {
+    throw error;
+  }))
+});
