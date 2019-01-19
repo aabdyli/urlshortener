8cc0d65980039462f20999c1c7abdfdc6730193d
Glitch (lily-chemistry) <none>
2019-01-19 11:27:14 +0000

    ⛸😺 Checkpoint ./server.js:1652815/835

--- server.js
+++ server.js
@@ -33,9 +33,10 @@ function urlCreate(data, callback) {
         })
         
         uri.save()
-          .then(
+          .then(saved => { callback(null, saved) })
+          .catch(err => { callback(err) })
       } else {
-        callback(null, data)
+        callback(null, foundUrl)
       }
     })
 }
@@ -77,21 +78,18 @@ app.post('/api/shorturl/new', function (req, res, next) {
     res.json({"error": "invalid URL"})
   }
 }, function (req, res) {
-     const short_url = Math.random().toString(36).substring(7);
- 
-     const uri = new Url({
-       original_url: req.body.url, 
-       short_url: short_url
-     })
- 
-     uri.save()
-       .then(uri => {
-         console.log(uri);
-       })
-       .catch(err => {
-         console.log(err);
-       });
-    res.json(uri)
+  urlCreate(req.body, function(err,data) {
+    if(err) {
+      console.log(err)
+    }
+    else {
+      res.json(data)
+    }
+  })
+});
+
+app.get("/api/shorturl/:uri", function (req,res) {
+  const uri = req.para
 });
 
 app.get("/api/hello", function (req, res) {
